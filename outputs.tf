d37153251b729186a6e5d3af91819d173c94fe9d
balusena <balusena12@gmail.com>
2024-01-19 23:36:06 +0000

    roboshop terraform files

--- outputs.tf
+++ outputs.tf
@@ -2,10 +2,6 @@ output "dns_name" {
   value = aws_lb.main.dns_name
 }
 
-#output "listener_arn" {
-#  value = aws_lb_listener.main.arn
-#}
-
 output "listener_arn" {
   value = var.name == "public" ? aws_lb_listener.main[0].arn : aws_lb_listener.private[0].arn
 }
